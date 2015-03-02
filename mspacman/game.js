dbcda7d21b53a8427cc075f9eab435841c00397f
limz <limz10@users.noreply.github.com>
2015-03-02 14:50:16 -0500

    JS to drawImage on the Canvas

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,14 @@
-// Replace this with your code...
+// Draw an image on the canvas
+
+function init() {
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext('2d');
+	var img = new Image();
+
+	img.addEventListener("load", function() {
+		ctx.drawImage(img, 322, 2, 464, 136, 0, 0, 464, 136);
+		ctx.drawImage(img, 82, 2, 18, 18, 7, 7, 18, 18);
+	}, false);
+
+	img.src = 'pacman10-hp-sprite.png';	
+}
