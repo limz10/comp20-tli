5fe7ffec5c1cea95ce89c8ad1225a628f5327326
limz <limz10@users.noreply.github.com>
2015-03-02 21:17:53 -0500

    image in center, README added

--- mspacman/game.js
+++ mspacman/game.js
@@ -6,8 +6,8 @@ function init() {
 	var img = new Image();
 
 	img.addEventListener("load", function() {
-		ctx.drawImage(img, 322, 2, 464, 136, 0, 0, 464, 136);
-		ctx.drawImage(img, 82, 2, 18, 18, 7, 7, 18, 18);
+		ctx.drawImage(img, 322, 2, 464, 136, 168, 232, 464, 136);
+		ctx.drawImage(img, 82, 2, 18, 18, 175, 239, 18, 18);
 	}, false);
 
 	img.src = 'pacman10-hp-sprite.png';	
