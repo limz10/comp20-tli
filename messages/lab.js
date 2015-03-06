b567e3cc58abc8fd4e9c8ecf34a5be921cf2e2cc
limz <limz10@users.noreply.github.com>
2015-03-06 00:46:51 -0500

    different data.json test

--- messages/lab.js
+++ messages/lab.js
@@ -10,17 +10,16 @@ function parse() {
 	data.send();
 
 	function displayMessages() {
-		console.log("In my displayMessages function " + data.readyState);
+		if (data.readyState == 4 && data.status == 200) {
+			messages = JSON.parse(data.responseText);
+			console.log(messages);	
 
-		messages = JSON.parse(data.responseText);
-		console.log(messages);	
+			var result = "";
+			for (i = 0; i < messages.length; i++) {
+				result += "<p>" + messages[i]['username'] + " : " + messages[i]["content"] + "</p>";
+			}
 
-		var result = "";
-		for (i = 0; i < messages.length; i++) {
-			result += "<p>" + messages[i]['username'] + " : " + messages[i]["content"] + "</p>";
+			document.getElementById("messages").innerHTML = result;
 		}
-
-		document.getElementById("messages").innerHTML = result;
 	}
 }
-
