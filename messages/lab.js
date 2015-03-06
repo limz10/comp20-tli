212899c374972b2323db36882110524582935c6d
limz <limz10@users.noreply.github.com>
2015-03-05 23:46:35 -0500

    open JSON

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+
+
+
+function parse() {
+
+	var data = new XMLHttpRequest();
+
+	data.open("get", data.json, true);
+	data.onreadystatechange = displayMessages;
+	data.send();
+
+	function displayMessages() {
+		messages = JSON.parse(data.responseText)
+			
+		document.getElementById("messages").innerHTML = data.responseText;
+	}
+}
+
