c64017c61d292533e5e582355b152f11d434d461
limz <limz10@users.noreply.github.com>
2015-03-05 23:55:02 -0500

    display messages

--- messages/lab.js
+++ messages/lab.js
@@ -12,6 +12,12 @@ function parse() {
 	function displayMessages() {
 		messages = JSON.parse(data.responseText)
 		console.log(messages);	
+
+		var result = "";
+		for (i = 0; i < messages.length; i++) {
+			result += "<p>" + messages[i]['username'] + " : " + messages[i]["content"] + "</p>";
+		}
+
 		document.getElementById("messages").innerHTML = data.responseText;
 	}
 }
