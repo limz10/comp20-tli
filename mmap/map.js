6541ba0bfd782d349a4c0beb7a003b979f773418
limz <limz10@users.noreply.github.com>
2015-03-07 15:26:39 -0500

    getting started

---
+++ mmap/map.js
@@ -0,0 +1,87 @@
+
+var myLat;
+var myLng;
+var request = new XMLHttpRequest();
+var me = new google.maps.LatLng(myLat, myLng);
+var myOptions = {
+	zoom: 13, // The larger the zoom number, the bigger the zoom
+	center: me,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map;
+var marker;
+var info_window = new google.maps.InfoWindow();
+var places;
+function init()
+{
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	console.log("Call before getMyLocation()");
+	getMyLocation();
+	console.log("Call after getMyLocation()");
+}
+function getMyLocation() {
+	console.log("In getMyLocation()");
+	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			renderMap();
+		});
+	}
+	else {
+		alert("Geolocation is not supported by your web browser. What a shame!");
+	}
+	console.log("Leaving getMyLocation()");
+}
+
+function renderMap()
+{
+	me = new google.maps.LatLng(myLat, myLng);
+	// Update map and go there...
+	map.panTo(me);
+	// Create a marker
+	marker = new google.maps.Marker({
+		position: me,
+		title: "Here I Am!"
+	});
+
+	marker.setMap(map);
+	// Open info window on click of marker
+	google.maps.event.addListener(marker, 'click', function() {
+		info_window.setContent(marker.title);
+		info_window.open(map, marker);
+	});
+	// Calling Google Places API
+var request = {
+	location: me,
+	radius: '500',
+	types: ['food']
+};
+service = new google.maps.places.PlacesService(map);
+service.search(request, callback);
+}
+// Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
+function callback(results, status)
+{
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		alert("Got places back!");
+		places = results;
+		for (var i = 0; i < results.length; i++) {
+			createMarker(results[i]);
+		}
+	}
+}
+function createMarker(place)
+{
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location
+	});
+	google.maps.event.addListener(marker, 'click', function() {
+		info_window.close();
+		info_window.setContent(place.name);
+		info_window.open(map, this);
+	});
+}
